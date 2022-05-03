2486a07f085345d90f0326262a53aa997a72e586
Christopher-at-694205907733 <cfox1290@gmail.com>
2022-05-03 10:17:02 -0500

    first pass on db schemas

--- resources/migrations/001_seed.sql
+++ resources/migrations/001_seed.sql
@@ -3,55 +3,68 @@
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-SET TIME ZONE 'UTC';
+CREATE TYPE TIMEMEASURE AS ENUM('minutes', 'hours', 'days', 'weeks', 'months', 'years');
 
--- organizations
-CREATE TABLE organizations(
-    id SERIAL PRIMARY KEY,
-    display_name VARCHAR(256) NOT NULL,
-    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    meta JSON DEFAULT '{}',
-    archived TIMESTAMP
-);
+SET TIME ZONE 'UTC';
 
 -- members
 CREATE TABLE members(
     id SERIAL PRIMARY KEY,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
+
+-- organizations
+CREATE TABLE organizations(
+    id SERIAL PRIMARY KEY,
+    display_name VARCHAR(256) NOT NULL,
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    meta JSON DEFAULT '{}',
+    archived TIMESTAMP
+);
+
 
 -- organization_members
 CREATE TABLE organization_members(
     organization INT NOT NULL REFERENCES organizations(id) ON DELETE CASCADE,
     member INT NOT NULL REFERENCES members(id) ON DELETE CASCADE,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     PRIMARY KEY (organization, member)
 );
 
 -- plant categories
 CREATE TABLE plant_categories(
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- plants
 CREATE TABLE plants(
-    id UUID PRIMARY KEY,
-    plant_category UUID NOT NULL REFERENCES plant_categories(id) ON DELETE CASCADE,
+    id SERIAL PRIMARY KEY,
+    plant_category INT NOT NULL REFERENCES plant_categories(id) ON DELETE CASCADE,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
@@ -59,10 +72,13 @@ CREATE TABLE plants(
 
 -- model_tasks
 CREATE TABLE model_tasks(
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     display_name VARCHAR(256) NOT NULL,
+    info TEXT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
@@ -70,70 +86,164 @@ CREATE TABLE model_tasks(
 -- generated_tasks
 CREATE TABLE generated_tasks(
     id UUID PRIMARY KEY,
-    task UUID NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
+    task INT NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    completed TIMESTAMPTZ,
+    completed_by INT REFERENCES members(id) ON DELETE CASCADE,
+    has_issue BOOLEAN DEFAULT FALSE,
+    reason TEXT,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- plant life cycles
 CREATE TABLE plant_life_cycles(
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    actual_time_measure NUMERIC,
     display_name VARCHAR(256) NOT NULL,
+    total_time_measure_units TIMEMEASURE NOT NULL,
+    est_total_time_measure INT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- plant stages
 CREATE TABLE plant_stages(
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    actual_time_measure NUMERIC,
     display_name VARCHAR(256) NOT NULL,
+    time_measure_units TIMEMEASURE NOT NULL,
+    est_time_measure INT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    completed TIMESTAMPTZ,
+    completed_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
+CREATE TABLE nutrients(
+    id SERIAL PRIMARY KEY,
+    display_name VARCHAR(256) NOT NULL,
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    meta JSON DEFAULT '{}',
+    archived TIMESTAMP
+);
+
+CREATE TABLE plant_stage_care(
+    id SERIAL PRIMARY KEY,
+    plant_stage INT NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    -- water (will generate task)
+    actual_water_time_measure NUMERIC,
+    water_time_measure_units TIMEMEASURE NOT NULL,
+    est_water_time_measure INT NOT NULL,
+    -- temperature (can generate task)
+    temperature_min INT,
+    temperature_max INT,
+    -- light (can generate task)
+    light_time_measure_hours INT,
+    -- observation (will generate task)
+    actual_observation_time_measure NUMERIC,
+    observation_time_measure_units TIMEMEASURE NOT NULL,
+    est_observation_time_measure INT NOT NULL,
+    -- time measure and 
+    actual_time_measure NUMERIC,
+    time_measure_units TIMEMEASURE NOT NULL,
+    est_time_measure INT NOT NULL,
+    -- over observation time (will generate task)
+    over_observation_time_measure_units TIMEMEASURE NOT NULL,
+    over_est_observation_time_measure INT NOT NULL,
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    meta JSON DEFAULT '{}',
+    archived TIMESTAMP
+);
+
+CREATE TABLE plant_stage_care_nutrients(
+    nutrient INT NOT NULL REFERENCES nutrients(id) ON DELETE CASCADE,
+    plant_stage INT NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    amount_units VARCHAR(16),
+    amount INT,
+    -- might make enum for this for trigger events
+    when_to_add VARCHAR(64), 
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    meta JSON DEFAULT '{}',
+    PRIMARY KEY (nutrient, plant_stage)
+);
+
 -- plant life cycle stages
 CREATE TABLE plant_life_cycle_stages(
-    plant_life_cycle UUID NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
-    plant_stage UUID NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    plant_life_cycle INT NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
+    plant_stage INT NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    stage_num INT NOT NULL,
+    prev_stage INT REFERENCES plant_stages(id) ON DELETE CASCADE,
+    next_stage INT REFERENCES plant_stages(id) ON DELETE CASCADE,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     PRIMARY KEY (plant_life_cycle, plant_stage)
 );
 
 -- plant stage tasks
 CREATE TABLE plant_stage_tasks(
-    model_task UUID NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
-    plant_stage UUID NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    model_task INT NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
+    plant_stage INT NOT NULL REFERENCES plant_stages(id) ON DELETE CASCADE,
+    actual_time_measure NUMERIC,
+    time_measure_units TIMEMEASURE NOT NULL,
+    est_time_measure INT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     PRIMARY KEY (model_task, plant_stage)
 );
 
 -- plant life cycle start tasks
 CREATE TABLE plant_life_cycle_start_tasks(
-    model_task UUID NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
-    plant_life_cycle UUID NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
+    model_task INT NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
+    plant_life_cycle INT NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
+    actual_time_measure NUMERIC,
+    time_measure_units TIMEMEASURE NOT NULL,
+    est_time_measure INT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     PRIMARY KEY (model_task, plant_life_cycle)
 );
 
 -- plant life cycle end tasks
 CREATE TABLE plant_life_cycle_end_tasks(
-    model_task UUID NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
-    plant_life_cycle UUID NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
+    model_task INT NOT NULL REFERENCES model_tasks(id) ON DELETE CASCADE,
+    plant_life_cycle INT NOT NULL REFERENCES plant_life_cycles(id) ON DELETE CASCADE,
+    actual_time_measure NUMERIC,
+    time_measure_units TIMEMEASURE NOT NULL,
+    est_time_measure INT NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     PRIMARY KEY (model_task, plant_life_cycle)
 );
@@ -143,7 +253,23 @@ CREATE TABLE devices(
     id UUID PRIMARY KEY,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
+    meta JSON DEFAULT '{}',
+    archived TIMESTAMP
+);
+
+CREATE TABLE device_actions(
+    id SERIAL PRIMARY KEY,
+    device UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
+    display_name VARCHAR(256) NOT NULL,
+    invoke_route VARCHAR(512) NOT NULL,
+    info TEXT NOT NULL,
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
@@ -154,21 +280,24 @@ CREATE TABLE growing_groups(
     organization INT NOT NULL REFERENCES organizations(id) ON DELETE CASCADE,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- growing group devices
 CREATE TABLE growing_group_devices(
-    id UUID PRIMARY KEY,
     device UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
     growing_group UUID NOT NULL REFERENCES growing_groups(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
-    archived TIMESTAMP
+    archived TIMESTAMP,
+    PRIMARY KEY (device, growing_group)
 );
 
 -- growing locations
@@ -177,21 +306,24 @@ CREATE TABLE growing_locations(
     growing_group UUID NOT NULL REFERENCES growing_groups(id) ON DELETE CASCADE,
     display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- growing location devices
 CREATE TABLE growing_location_devices(
-    id UUID PRIMARY KEY,
     device UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
     growing_location UUID NOT NULL REFERENCES growing_locations(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
-    archived TIMESTAMP
+    archived TIMESTAMP,
+    PRIMARY KEY (device, growing_location)
 );
 
 -- growing levels
@@ -199,50 +331,63 @@ CREATE TABLE growing_levels(
     id UUID PRIMARY KEY,
     growing_location UUID NOT NULL REFERENCES growing_locations(id) ON DELETE CASCADE,
     display_name VARCHAR(256) NOT NULL,
+    level_num INT DEFAULT 1,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- growing level devices
 CREATE TABLE growing_level_devices(
-    id UUID PRIMARY KEY,
     device UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
     growing_level UUID NOT NULL REFERENCES growing_levels(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
-    archived TIMESTAMP
+    archived TIMESTAMP,
+    PRIMARY KEY (device, growing_level)
 );
 
 -- grow spots
 CREATE TABLE grow_spots(
     id UUID PRIMARY KEY,
-    grow_spot UUID NOT NULL REFERENCES grow_spots(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
+    growing_level UUID NOT NULL REFERENCES growing_levels(id) ON DELETE CASCADE,
+    is_individual BOOLEAN DEFAULT TRUE, 
+    is_companion BOOLEAN DEFAULT FALSE,
+    num_spots INT DEFAULT 1,
+    spot_type VARCHAR(64),
+    spot_size VARCHAR(16),
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
     archived TIMESTAMP
 );
 
 -- grow spot devices
 CREATE TABLE grow_spots_devices(
-    id UUID PRIMARY KEY,
     device UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
     grow_spot UUID NOT NULL REFERENCES grow_spots(id) ON DELETE CASCADE,
-    display_name VARCHAR(256) NOT NULL,
     created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by INT REFERENCES members(id) ON DELETE CASCADE,
     updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_by INT REFERENCES members(id) ON DELETE CASCADE,
     meta JSON DEFAULT '{}',
-    archived TIMESTAMP
+    archived TIMESTAMP,
+    PRIMARY KEY (device, grow_spot)
 );
 
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
+
+DROP TYPE IF EXISTS TIMEMEASURE CASCADE;
 
 DROP TABLE IF EXISTS organizations CASCADE;
 DROP TABLE IF EXISTS members CASCADE;
@@ -253,11 +398,15 @@ DROP TABLE IF EXISTS model_tasks CASCADE;
 DROP TABLE IF EXISTS generated_tasks CASCADE;
 DROP TABLE IF EXISTS plant_life_cycles CASCADE;
 DROP TABLE IF EXISTS plant_stages CASCADE;
+DROP TABLE IF EXISTS nutrients CASCADE;
+DROP TABLE IF EXISTS plant_stage_care CASCADE
+DROP TABLE IF EXISTS plant_stage_care_nutrients CASCADE
 DROP TABLE IF EXISTS plant_life_cycle_stages CASCADE;
 DROP TABLE IF EXISTS plant_stage_tasks CASCADE;
 DROP TABLE IF EXISTS plant_life_cycle_start_tasks CASCADE;
 DROP TABLE IF EXISTS plant_life_cycle_end_tasks CASCADE;
 DROP TABLE IF EXISTS devices CASCADE;
+DROP TABLE IF EXISTS device_actions CASCADE;
 DROP TABLE IF EXISTS growing_groups CASCADE;
 DROP TABLE IF EXISTS growing_group_devices CASCADE;
 DROP TABLE IF EXISTS growing_locations CASCADE;
--- resources/migrations/002_seed_funcs.sql
+++ resources/migrations/002_seed_funcs.sql
@@ -11,28 +11,8 @@ BEGIN
 END;$$ LANGUAGE plpgsql;
 -- +migrate StatementEnd
 
-CREATE TRIGGER new_plant_category_id
-BEFORE INSERT ON plant_categories
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_plants_id
-BEFORE INSERT ON plants
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_model_tasks_id
-BEFORE INSERT ON model_tasks
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
 CREATE TRIGGER new_generated_tasks_id
 BEFORE INSERT ON generated_tasks
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_plant_life_cycles_id
-BEFORE INSERT ON plant_life_cycles
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_plant_stages_id
-BEFORE INSERT ON plant_stages
 FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
 CREATE TRIGGER new_devices_id
@@ -43,32 +23,16 @@ CREATE TRIGGER new_growing_groups_id
 BEFORE INSERT ON growing_groups
 FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
-CREATE TRIGGER new_growing_group_devices_id
-BEFORE INSERT ON growing_group_devices
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
 CREATE TRIGGER new_growing_locations_id
 BEFORE INSERT ON growing_locations
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_growing_location_devices_id
-BEFORE INSERT ON growing_location_devices
 FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
 CREATE TRIGGER new_growing_levels_id
 BEFORE INSERT ON growing_levels
 FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
-CREATE TRIGGER new_growing_level_devices_id
-BEFORE INSERT ON growing_level_devices
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
 CREATE TRIGGER new_grow_spots_id
 BEFORE INSERT ON grow_spots
-FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
-
-CREATE TRIGGER new_grow_spots_devices_id
-BEFORE INSERT ON grow_spots_devices
 FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
 -- +migrate Down
@@ -76,18 +40,9 @@ FOR EACH ROW EXECUTE PROCEDURE inject_uuid();
 
 DROP FUNCTION inject_uuid CASCADE;
 
-DROP TRIGGER IF EXISTS new_plant_category_id ON plant_categories;
-DROP TRIGGER IF EXISTS new_plants_id ON plants;
-DROP TRIGGER IF EXISTS new_model_tasks_id ON model_tasks;
 DROP TRIGGER IF EXISTS new_generated_tasks_id ON generated_tasks;
-DROP TRIGGER IF EXISTS new_plant_life_cycles_id ON plant_life_cycles;
-DROP TRIGGER IF EXISTS new_plant_stages_id ON plant_stages;
 DROP TRIGGER IF EXISTS new_devices_id ON devices;
 DROP TRIGGER IF EXISTS new_growing_groups_id ON growing_groups;
-DROP TRIGGER IF EXISTS new_growing_group_devices_id ON growing_group_devices;
 DROP TRIGGER IF EXISTS new_growing_locations_id ON growing_locations;
-DROP TRIGGER IF EXISTS new_growing_location_devices_id ON growing_location_devices;
 DROP TRIGGER IF EXISTS new_growing_levels_id ON growing_levels;
-DROP TRIGGER IF EXISTS new_growing_level_devices_id ON growing_level_devices;
 DROP TRIGGER IF EXISTS new_grow_spots_id ON grow_spots;
-DROP TRIGGER IF EXISTS new_grow_spots_devices_id ON grow_spots_devices;
