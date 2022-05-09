2dcf096bf4e56700bf93a761f0287a50717d313a
Christopher-at-694205907733 <cfox1290@gmail.com>
2022-05-09 07:39:13 -0500

    moving growing medium sql

--- pkg/internal/persistence/growing_medium.go
+++ pkg/internal/persistence/growing_medium.go
@@ -12,13 +12,6 @@ func (db *Persistence) CreateGrowingMedium(req *routemodels.CreateGrowingMediumR
 	/**********************************************************************/
 	var result string
 
-	SQL := `
-	INSERT INTO growing_mediums
-	(display_name)
-	VALUES ($1)
-	RETURNING id
-	`
-
 	/**********************************************************************
 	/
 	/	Define Arguments For SQL Call
@@ -35,7 +28,7 @@ func (db *Persistence) CreateGrowingMedium(req *routemodels.CreateGrowingMediumR
 	/
 	/**********************************************************************/
 
-	if err := db.Postgres.QueryRow(SQL, args...).Scan(result); err != nil {
+	if err := db.Postgres.QueryRow(CREATE_GROWING_MEDIUM_SQL, args...).Scan(result); err != nil {
 		// handle err
 		return nil, err
 	}
@@ -58,11 +51,6 @@ func (db *Persistence) DeleteGrowingMedium(req *routemodels.DeleteGrowingMediumR
 	/**********************************************************************/
 	var result string
 
-	SQL := `
-	UPDATE growing_mediums SET archived = NOW()
-	WHERE growing_mediums.id = $1
-	`
-
 	/**********************************************************************
 	/
 	/	Define Arguments For SQL Call
@@ -79,7 +67,7 @@ func (db *Persistence) DeleteGrowingMedium(req *routemodels.DeleteGrowingMediumR
 	/
 	/**********************************************************************/
 
-	if err := db.Postgres.QueryRow(SQL, args...).Scan(result); err != nil {
+	if err := db.Postgres.QueryRow(DELETE_GROWING_MEDIUM_SQL, args...).Scan(result); err != nil {
 		// handle err
 		return err
 	}
@@ -129,22 +117,6 @@ func (db *Persistence) GetGrowingMedium(req *routemodels.GetGrowingMediumRequest
 	/**********************************************************************/
 	var result routemodels.GrowingMedium
 
-	SQL := `
-	SELECT
-		growing_mediums.id AS id,
-		growing_mediums.display_name AS display_name,
-		growing_mediums.created AS created_at,
-		growing_mediums.updated AS updated_at,
-		cre_member.id AS created_member_id,
-		cre_member.display_name AS created_member_name,
-		up_member.id AS updated_member_id,
-		up_member.display_name AS updated_member_name
-	FROM growing_mediums
-	LEFT JOIN members AS cre_member ON members.id = growing_mediums.created_by
-	LEFT JOIN members AS up_member ON members.id = growing_mediums.updated_by
-	WHERE growing_mediums.id = $1
-	`
-
 	/**********************************************************************
 	/
 	/	Define Arguments For SQL Call
@@ -161,7 +133,7 @@ func (db *Persistence) GetGrowingMedium(req *routemodels.GetGrowingMediumRequest
 	/
 	/**********************************************************************/
 
-	if err := db.Postgres.Get(&result, SQL, args...); err != nil {
+	if err := db.Postgres.Get(&result, GET_GROWING_MEDIUM_BY_ID_SQL, args...); err != nil {
 		// handle err
 		return nil, err
 	}
@@ -184,21 +156,6 @@ func (db *Persistence) GetAllGrowingMediums(req *routemodels.GetAllGrowingMedium
 	/**********************************************************************/
 	var result []routemodels.GrowingMedium
 
-	SQL := `
-	SELECT
-		growing_mediums.id AS id,
-		growing_mediums.display_name AS display_name,
-		growing_mediums.created AS created_at,
-		growing_mediums.updated AS updated_at,
-		cre_member.id AS created_member_id,
-		cre_member.display_name AS created_member_name,
-		up_member.id AS updated_member_id,
-		up_member.display_name AS updated_member_name
-	FROM growing_mediums
-	LEFT JOIN members AS cre_member ON members.id = growing_mediums.created_by
-	LEFT JOIN members AS up_member ON members.id = growing_mediums.updated_by
-	`
-
 	/**********************************************************************
 	/
 	/	Define Arguments For SQL Call
@@ -213,7 +170,7 @@ func (db *Persistence) GetAllGrowingMediums(req *routemodels.GetAllGrowingMedium
 	/
 	/**********************************************************************/
 
-	if err := db.Postgres.Select(&result, SQL, args...); err != nil {
+	if err := db.Postgres.Select(&result, GET_ALL_GROWING_MEDIUM_SQL, args...); err != nil {
 		// handle err
 		return nil, err
 	}
